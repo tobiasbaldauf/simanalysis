72304c29b318f5d8fa9ae2d1e933a0f61f153b17
Tobias Cosmos <t.baldauf@tbaweb.de>
2018-03-26 17:59:11 +0100

    bispect mu

--- bispect_mmh_init.f90
+++ bispect_mmh_init.f90
@@ -1,14 +1,36 @@
-program power
+module HaloTYPE
 
+  type Halo
+
+     integer  :: HN                       ! number of particles in the halo                  
+     real(8) :: Hmass                    ! Halo Mass fof=0.2 [10**10 Mo/h]
+
+     real(8), dimension(3) :: HPos       ! centre of mass position [Mpc/h]
+     real(8), dimension(3) :: HVel       ! centre of mass velocity [km/s]
+
+     real(8) :: Hrad                     ! radius of halo = mean of last 4 part.
+     real(8) :: Hradhalf                 ! Half-mass radius
+     real(8) :: HDisp1D                  ! 1D velocity dispersion [km/s]
+
+     real(8), dimension(6) :: HInert     ! Inertia matrix: 
+                                          ! I11, I22, I33, I12, I13, I23
+  end type Halo
+
+end module HaloTYPE
+
+program power
+USE HaloTYPE
 USE eval           ! Frequently used Routines for Correlation and Power Spectrum
 USE snapshot       ! Reads in Dark Matter Particles 
 implicit none
+
 
 
 ! All that in and out Files
 character*200 :: Folder
 character*200 :: InFileBase
 character*200 :: PsFileBase, PsOutFile,Extension
+character*200 :: datadir,HalFileBase,HalInFile,HaloFileBase
 character*10 :: nodestr,snapstr,ncstr
 
 integer :: idat                ! =1 read data; =0 get header
@@ -18,14 +40,15 @@ integer :: iID                 ! =1 read the ID's; =0 do not.
 integer :: iRed                ! =1 no redshift space distortions
                                  ! = (2,3,4) apply distortion in (x-,y-,z-) 
 type(snapFILES) :: sF
-	
+type(halo) :: HaloFINAL	
+
 ! Switches
 integer :: doCorrect						! do CIC correction or not
 integer :: NodeNumber
 integer :: FileNumber
 	
 ! Box dimensions
-real(8), dimension(:,:,:), allocatable :: deltadm,deltah
+real(8), dimension(:,:,:), allocatable :: deltadm,deltag
 real(8), dimension(:,:,:), allocatable :: deltar
 
 integer, dimension(NkBins) :: kBinCnt
@@ -41,10 +64,20 @@ integer :: OMP_GET_NUM_THREADS,TID,OMP_GET_THREAD_NUM
 
 integer :: i
 
+integer :: NHalBin,iMBin,cnt,Nhaltot
+integer, parameter :: NMassBins=5
+real, dimension(NMassBins+1) :: MBinB
+integer, dimension(NMassBins+1) :: NBinB
+real, dimension(NMassBins) :: MBinC
+integer, dimension(NMassBins) :: NHalosBin
+integer, dimension(:), allocatable :: IndM
+real(4), dimension(:), allocatable :: HalM
+real(8), dimension(:,:), allocatable :: HalPos,HalVel
 
 
 
-	
+
+NbinB=(/20,60,180,540,1620,4860/) ! Old bins	
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	
 call cpu_time(time1)
@@ -88,6 +121,11 @@ endif
 
 doCorrect=1
 
+
+datadir='/slow/space/cosmos/lss-nongauss/baldauf/SimSuite/wmap7_fid_run'//trim(nodestr)//'/DATA/reducedGROUPS/'
+!HalInFile=trim(datadir)//'GRPS_TrBack002_CMS_wmap7_fid_run'//trim(nodestr)//'_'//trim(snapstr)
+HalInFile=trim(datadir)//'GRPS_CMS_wmap7_fid_run'//trim(nodestr)//'_'//trim(snapstr)
+write(*,'(a)') HalInFile
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ! Output File Names
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -140,7 +178,7 @@ call comprod(deltadm,deltadm,deltar)
 call powerspectrum(deltar,kBinCnt,kTrueBinC,powerdfdf)	
 deallocate(deltar)
 
-! Write Output
+! Write Power Spectrum Output
 open(20,file=PsOutFile,form='formatted',status='replace')
     do i=1,NkBins
 	write(20,'(2ES30.10,1I30,1ES30.10)') kbinc(i),ktruebinc(i),kbincnt(i),powerdfdf(i)
@@ -148,18 +186,87 @@ open(20,file=PsOutFile,form='formatted',status='replace')
 close(20)
 
 
+	do iMBin=1,5
+		write(*,'(a)') '\n\n ***********************************************'
+		write(*,'(a)') '          > > > Halos Mass-Binned < < <'
+		write(*,'(a)') ' ***********************************************\n\n'
+		
+		open(11,file=trim(HalInFile),status='old',form='unformatted')
+		read(11) NHalTot
+		
+		print*,'NHalTot',NHalTot
+		cnt=0
+		do i=1,NHalTot
+			read(11) HaloFINAL
+			if (HaloFINAL%HN>=NBinB(iMBin) .and. HaloFINAL%HN<NBinB(iMBin+1)) then
+					cnt=cnt+1
+			endif
+		end do
+		close(11)
+		NHalBin=cnt
+		print*,'NHalBin',NHalBin
 
-!load haloes
+		allocate(HalM(NHalBin))
+		allocate(HalPos(3,NHalBin))
+		allocate(HalVel(3,NHalBin))
+		
+		
+		
+		open(11,file=trim(HalInFile),status='old',form='unformatted')
+		read(11) NHalTot
+		cnt=0
+		do i=1,NHalTot
+			read(11) HaloFINAL
+			if (HaloFINAL%HN>=NBinB(iMBin) .and. HaloFINAL%HN<NBinB(iMBin+1)) then
+			    cnt=cnt+1
+			    HalM(cnt)=HaloFINAL%HMass
+			    HalPos(:,cnt)=HaloFINAL%HPos(:)
+			    HalVel(:,cnt)=HaloFINAL%HVel(:)/sqrt(0.0d0+1.0d0)
+			endif
+		end do
+		close(11)
+		
+		print*,'Haloes Loaded'
+		
+			allocate(deltag(1:NCell+2,1:NCell,1:NCell))
+			!allocate(thetag(1:NCell+2,1:NCell,1:NCell))
+			
+			deltag=0.d0
+			call cicmass(NHalBin,real(HalPos),deltag)
+			MBinC(iMBin)=sum(HalM)/real(NHalBin)
 
-print*,'Calling Bispectrum routine'
-call bispectrum_mu_threefield(deltadm,deltadm,deltah,.False.,20,bispectcnt,bispectdmdmdh,bispectk)
+			call normalize(deltag)
+			
 
-call bispectmuhdf5export('Spectra/bispect_hr_NODE'//trim(nodestr)//'.h5','bispect',bispectcnt,bispectdmdmdh,bispectk)
+			call fft3d(deltag,deltag,'f')
+			
+		
+			if (doCorrect==1) then
+ 				call ciccorrect(deltag,1.0d0)
+			endif
+		
+			print*,'Calling Bispectrum routine'
+			call bispectrum_mu_threefield(deltadm,deltadm,deltag,.False.,20,bispectcnt,bispectdmdmdh,bispectk)
+
+			call bispectmuhdf5export('Spectra/bispect_hr_NODE'//trim(nodestr)//'.h5','bispect',bispectcnt,bispectdmdmdh,bispectk)
 
 
-do i=1,nkbins
-write(*,'(1i10,3f12.2,1es20.4)'), bispectcnt(i,i,i),bispectk(i,i,i,:)/((2.0*pi)/box),bispectdmdmdh(i,i,i)
-enddo
+			do i=1,nkbins
+				write(*,'(1i10,3f12.2,1es20.4)'), bispectcnt(i,i,i),bispectk(i,i,i,:)/((2.0*pi)/box),bispectdmdmdh(i,i,i)
+			enddo
+
+					
+			deallocate(deltag)
+			
+			deallocate(HalM)
+			deallocate(HalVel)
+			deallocate(HalPos)
+	
+		enddo
+	
+
+
+
 
 				
 deallocate(deltadm)
